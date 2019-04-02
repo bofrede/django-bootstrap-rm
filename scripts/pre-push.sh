de3d5be3e9ec3dd3086d08f6a1d0975944b724c1
root@localhost <root@localhost>
2019-04-02 09:53:43 -0300

    removed redundant files

--- scripts/pre-push.sh
+++ scripts/pre-push.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 set -e
 DIR=$(dirname "$0")
-cd ${DIR}/..
+cd ${DIR}/../..
 
 echo "Running flake8"
 pipenv run flake8
 echo "flake8 OK :)"
 
 echo "Running pylint"
-pipenv run pylint --load-plugins pylint_django project/
+pipenv run pylint project/
 echo "pylint OK :)"
 
 echo "Running py.test"
